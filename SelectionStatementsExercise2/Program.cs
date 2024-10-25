d0c19ea5156a4b02872b0e1b674e7ba08feda1a4
Riley Plokhooy <rileyanthony2006@gmail.com>
2024-10-25 13:30:30 -0400

    Completed second part

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject() 
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower()) 
+            {
+                case "math":
+                    Console.WriteLine("I think math is okay");
+                    break;
+                case "science":
+                    Console.WriteLine("As Bill Nye once said: Science Rules");
+                    break;
+                case "pe":
+                case "p.e":
+                case "physical education":
+                    Console.WriteLine("P.E. is fun on occasion");
+                    break;
+                case "history":
+                    Console.WriteLine("History is boring in my opinion");
+                    break;
+                case "geography":
+                    Console.WriteLine("It's okay I guess");
+                    break;
+                case "lunch":
+                    Console.WriteLine("You are a genius");
+                    break;
+                default:
+                    Console.WriteLine("Im going to pretend that I wrote code for that as well. Thats a great subject!");
+                    break;
+            }
         }
     }
 }
